10c68ac169a6dc6e77c9c2ac14ee36d4b3d18dfc
Jasdeep Jaitla <jasdeep@scalabl3.com>
2012-08-26 14:17:46 -0700

    updating pages

---
+++ index.rb
@@ -0,0 +1,7 @@
+# Quick documentation on the Radlibs Model
+
+#Analytics.rb 
+# Analytics class contains all the initializers for analytics tracking and
+#the global user_count and radlibs_count. Also initializes the Couchbase document
+#store
+<a href="analytics.html">Analytics.rb</a>
